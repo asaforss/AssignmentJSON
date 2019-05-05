1a60ed3f11295c5cbc7619df9e38a38fbd32f357
Åsa Roshällen <asa.maria.forss@gmail.com>
2019-05-05 19:09:56 +0200

    Cleaned out not used usings

--- DeliveryDate.cs
+++ DeliveryDate.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Runtime.Serialization;
-using System.Xml;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
 
 namespace Assignement
     
